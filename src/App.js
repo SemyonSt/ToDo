b9939c1e5c0b466c1bb460b83828b14f617c50eb
Семён <sema.stulnikov@mail.ru>
2022-08-19 23:01:26 +0300

    changeTaskName

--- src/App.js
+++ src/App.js
@@ -27,6 +27,16 @@ function App() {
     setLists(newList);
   };
 
+  const onEditListTitle = (id, title) => { //Меняем заголовок задачи
+    const newList = lists.map(item => {
+      if(item.id === id) {
+        item.name = title;
+      }
+      return item
+    })
+    setLists(newList);
+  }
+
   return (
     <div className="todo">
       <div className="todo__sidebar">
@@ -71,7 +81,7 @@ function App() {
         <AddList onAdd={onAddList} colors={colors} />
 
       </div>
-      <div className="todo__tasks">{lists && activeItem && <Tasks list={activeItem} />}</div>
+      <div className="todo__tasks">{lists && activeItem && <Tasks list={activeItem} onEditTitle={onEditListTitle}/>}</div>
 
     </div>
   );
--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -1,19 +1,33 @@
+import axios from "axios";
 import React from "react";
 import editSvg from '../../assets/img/edit.svg'
 
 import './Tasks.scss';
 
 
-const Tasks = ({list}) => {
-    console.log(list)
+const Tasks = ({list, onEditTitle}) => {
+    const editTitle = () => {
+        const newTitle = window.prompt('Название списка', list.name);
+        if(newTitle) {
+            onEditTitle(list.id, newTitle);
+            axios
+                .patch('http://localhost:3001/lists/' + list.id, {
+                    name: newTitle
+                })
+                .catch(() => {
+                    alert('Не удалось обновить название списка')
+                })
+        }
+    }
     return (
         <div className='tasks'>
             <h2 className='tasks__title'>
                 {list.name}
-                <img src={editSvg} alt="Edit icon" />
+                <img onClick={editTitle} src={editSvg} alt="Edit icon" />
             </h2>
 
             <div className="tasks__items">
+                {!list.tasks.length && <h2>Задачи отсутствуют</h2>}
                 {
                     list.tasks.map(task => (
                         <div key={task.id} className="tasks__items-row">
